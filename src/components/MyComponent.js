4402d4903c93c023bb60dfe61dfd79f1c969a2e9
Hoan-giau-ten <BDHoan0406@gmail.com>
2024-01-27 00:54:51 +0700

    vip2

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,12 +2,16 @@
 // function component
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "Hoan_giau_ten",
+    address: "HoiDanIT",
+    age: 22,
+  };
   //JSX
   render() {
     return (
       <div>
-        my first component
-        {Math.random()}
+        My name is {this.state.name} and I am form {this.state.address}
       </div>
     );
   }
