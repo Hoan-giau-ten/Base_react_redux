e4aba7c9998520647e444e32a03885609b910f9d
Hoan-giau-ten <BDHoan0406@gmail.com>
2024-01-28 22:53:28 +0700

    vip7

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,15 +2,21 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
   render() {
-    console.log(this.props);
     // destructuring array/object
-    const { age, name } = this.props;
+    const { listUsers } = this.props;
+    console.log(listUsers);
     // props => viết tắt của properties
     return (
       <div>
-        DisplayInfor
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name is {user.name}</div>
+              <div>My age is {user.age}</div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,17 +4,21 @@ import React from "react";
 import Userinfor from "./Userinfor";
 import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Hoan_giau_ten", age: 22 },
+      { id: 2, name: "Hoan", age: 18 },
+      { id: 3, name: "Hoan_Bui", age: 25 },
+    ],
+  };
   //JSX
   render() {
-    const myInfo = ["ab", "bc", "cd"];
-
+    //DRY: don't repert yourseft
     return (
       <div>
         <Userinfor />
         <br /> <br />
-        <DisplayInfor name="HoiDanIT" age="22" />
-        <hr />
-        <DisplayInfor name="Hoan_giau_ten" age={18} myInfo={myInfo} />
+        <DisplayInfor listUsers={this.state.listUsers} />
       </div>
     );
   }
