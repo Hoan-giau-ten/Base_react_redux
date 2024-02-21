bbbc9ef41f730df84e39408e5f6ca41a33f68542
Hoan-giau-ten <BDHoan0406@gmail.com>
2024-02-21 18:08:22 +0700

    vip14

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,79 +2,72 @@ import React from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  // Hàm ẩn hiển (chú ý có dấu !)
+// class DisplayInfor extends React.Component {
+//   // Hàm ẩn hiển (chú ý có dấu !)
 
-  // React class, phải có hàm constructor mới là chuẩn
-  constructor(props) {
-    console.log(">>> call constructor; 1");
-    super(props);
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+//   // React class, phải có hàm constructor mới là chuẩn
 
-  componentDidMount() {
-    console.log(">>> Call me component did mount");
-    setTimeout(() => {
-      document.title = "HoiDanIT";
-    }, 3000);
-  }
+//   render() {
+//     console.log(">>call me render:");
+//     // destructuring array/object
+//     const { listUsers } = this.props;
+//     // console.log(listUsers);
+//     // props => viết tắt của properties
+//     return (
+//       <div className="display-infor-container">
+//         {true && (
+//           <>
+//             {listUsers.map((user) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>
+//                     <div>My name is {user.name}</div>
+//                     <div>My age is {user.age}</div>
+//                   </div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(">>> call me component did update:", this.props, prevProps);
-  }
+const DisplayInfor = (props) => {
+  const { listUsers } = props;
 
-  handleShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
-  };
-
-  render() {
-    console.log(">>call me render:");
-    // destructuring array/object
-    const { listUsers } = this.props;
-    // console.log(listUsers);
-    // props => viết tắt của properties
-    return (
-      <div className="display-infor-container">
-        <img src={logo} />
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUser === true
-              ? "Hide list users: "
-              : "Show list user:"}
-          </span>
-        </div>
-        {this.state.isShowListUser && (
-          <div>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>
-                    <div>My name is {user.name}</div>
-                    <div>My age is {user.age}</div>
-                  </div>
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                  <hr />
+  return (
+    <div className="display-infor-container">
+      {true && (
+        <>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>
+                  <div>My name is {user.name}</div>
+                  <div>My age is {user.age}</div>
                 </div>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
+                </div>
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 export default DisplayInfor;
